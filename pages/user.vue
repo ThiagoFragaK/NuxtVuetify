c25b0ff098ab8aaa29e7d5a7e7d7cf407963aef5
ThiagoFragaK <thiago@fragainformatica.com>
2022-05-01 21:22:56 -0300

    Added: Users Info

--- pages/user.vue
+++ pages/user.vue
@@ -2,9 +2,16 @@
     <v-container class="dark lighten-5">
         <v-row>
             <v-col cols="12" sm="12">
-            <v-card class="pa-11" outlined tile >
-                <h1 class="font-weight-black text-center">{{ user.personaname }}</h1>
-            </v-card>
+                <v-card class="pa-11" outlined tile >
+                    <h1 class="font-weight-black text-center">{{ user.personaname }}</h1>
+                    <hr>
+                    <br/>
+                    <v-row align="center" justify="space-around" >
+                        <v-btn text disabled>Steam Id: {{ user.steamid }}</v-btn>
+                        <v-btn text disabled>Real Name: {{ user.realname }}</v-btn>
+                        <v-btn text to="this.profileUrl()">Steam Profile</v-btn>
+                    </v-row>
+                </v-card>
             </v-col>
         </v-row>    
     </v-container>  
@@ -15,10 +22,12 @@ export default {
     async asyncData({ params, $axios }) {
         const user = await $axios.$get('api/v001/user');
         return { user }
+    },
+    profileUrl(){
+        return user.profileurl;
     }
 }
 </script>
 
 <style>
-
 </style>
